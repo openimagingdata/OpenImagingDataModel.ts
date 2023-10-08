559dd74dc45a3d8ce6335e2a02aa2f9b0c384195
Tarik Alkasab <tarik@alkasab.org>
2023-10-08 14:03:25 +0000

    Testing on real RadElement data

--- packages/cde_set/src/types/cdeSet.test.ts
+++ packages/cde_set/src/types/cdeSet.test.ts
@@ -1,8 +1,10 @@
 import { describe, it, expect } from 'vitest';
 
+import fs from 'fs';
+
 import { CdeSetData, cdeSetSchema, CdeSet } from './cdeSet';
 
-import { Organization, Person } from './shared';
+import { Person } from './shared';
 
 const cdeSetJson: CdeSetData = {
   id: 'RDES231',
@@ -224,5 +226,212 @@ describe('cdeSet', () => {
       'href',
       'http://www.radlex.org/RID/RID6434'
     );
+  });
+
+  const ribFractureJson: CdeSetData = {
+    id: 'RDES112',
+    name: 'Rib Fracture on Chest CT',
+    description: 'Rib fracture identification- CT chest in emergency setting',
+    version: {
+      name: 'Rib Fracture on Chest CT',
+      version_date: '00/00/0000',
+      status: 'Proposed',
+    },
+    url: 'https://api3.rsna.org/radelement/public/sets/112',
+    index_codes: [],
+    body_parts: [
+      {
+        name: 'Rib',
+        index_codes: {
+          system: 'RADLEX',
+          code: 'RID2471',
+          display: 'Rib',
+          href: 'http://www.radlex.org/RID/RID2471',
+        },
+      },
+    ],
+    authors: {
+      person: [
+        {
+          name: 'Jordan Meyer',
+          orcid_id: null,
+          twitter_handle: null,
+          url: null,
+          role: null,
+        },
+      ],
+      organization: [],
+    },
+    history: [],
+    specialty: [
+      { abbreviation: 'CH', name: 'Chest Radiology' },
+      { abbreviation: 'ER', name: 'Emergency Radiology' },
+    ],
+    elements: [
+      {
+        id: 'RDE713',
+        parent_id: 112,
+        name: 'Displacement',
+        short_name: '',
+        editor: '',
+        instructions: '',
+        synonyms: '',
+        definition: 'Total euclidean displacement of a rib fracture\n',
+        question: '',
+        version: {
+          name: '',
+          version_date: '12/06/2022',
+          status_date: '12/06/2022',
+          status: 'Proposed',
+        },
+        index_codes: [],
+        authors: { person: [], organization: [] },
+        history: [],
+        specialty: [],
+        references: [],
+        source: '',
+        float_values: {
+          cardinality: { min_cardinality: 1, max_cardinality: 1 },
+          value_min_max: { value_min: null, value_max: null },
+          step_value: 0.1,
+          unit: 'mm',
+          value_type: 'float',
+          value_size: 0,
+          values: [],
+        },
+      },
+      {
+        id: 'RDE714',
+        parent_id: 112,
+        name: 'Count',
+        short_name: '',
+        editor: '',
+        instructions: '',
+        synonyms: '',
+        definition: 'number of rib fractures total',
+        question: '',
+        version: {
+          name: '',
+          version_date: '12/06/2022',
+          status_date: '12/06/2022',
+          status: 'Proposed',
+        },
+        index_codes: [],
+        authors: { person: [], organization: [] },
+        history: [],
+        specialty: [],
+        references: [],
+        source: '',
+        integer_values: {
+          cardinality: { min_cardinality: 1, max_cardinality: 1 },
+          value_min_max: { value_min: null, value_max: null },
+          step_value: 1,
+          unit: 'rib fractures',
+          value_type: 'integer',
+          value_size: 0,
+          values: [],
+        },
+      },
+      {
+        id: 'RDE715',
+        parent_id: 112,
+        name: 'Presence',
+        short_name: '',
+        editor: '',
+        instructions: '',
+        synonyms: '',
+        definition: 'detection of rib fracture',
+        question: '',
+        version: {
+          name: '',
+          version_date: '02/06/2023',
+          status_date: '02/06/2023',
+          status: 'Proposed',
+        },
+        index_codes: [],
+        authors: { person: [], organization: [] },
+        history: [],
+        specialty: [],
+        references: [],
+        source: '',
+        value_set: {
+          cardinality: { min_cardinality: 1, max_cardinality: 1 },
+          value_min_max: { value_min: null, value_max: null },
+          step_value: null,
+          unit: '',
+          value_type: 'valueSet',
+          value_size: 0,
+          values: [
+            {
+              value: 'RDE715.0',
+              name: 'absent',
+              definition: 'fracture absent',
+            },
+            {
+              value: 'RDE715.1',
+              name: 'present',
+              definition: 'fracture present',
+            },
+            { value: 'RDE715.2', name: 'unknown', definition: 'unknown' },
+            { value: 'RDE715.3', name: 'indeterminate', definition: '' },
+          ],
+        },
+      },
+      {
+        id: 'RDE717',
+        parent_id: 112,
+        name: 'Affected rib',
+        short_name: '',
+        editor: '',
+        instructions: '',
+        synonyms: '',
+        definition: 'Enumerated rib(s) with fracture',
+        question: '',
+        version: {
+          name: '',
+          version_date: '02/06/2023',
+          status_date: '02/06/2023',
+          status: 'Proposed',
+        },
+        index_codes: [],
+        authors: { person: [], organization: [] },
+        history: [],
+        specialty: [],
+        references: [],
+        source: '',
+        value_set: {
+          cardinality: { min_cardinality: 1, max_cardinality: 12 },
+          value_min_max: { value_min: null, value_max: null },
+          step_value: null,
+          unit: '',
+          value_type: 'valueSet',
+          value_size: 0,
+          values: [
+            { value: 'RDE717.0', name: '1', definition: '' },
+            { value: 'RDE717.1', name: '2', definition: '' },
+            { value: 'RDE717.2', name: '3', definition: '' },
+            { value: 'RDE717.3', name: '4', definition: '' },
+            { value: 'RDE717.4', name: '5', definition: '' },
+            { value: 'RDE717.5', name: '6', definition: '' },
+            { value: 'RDE717.6', name: '7', definition: '' },
+            { value: 'RDE717.7', name: '8', definition: '' },
+            { value: 'RDE717.8', name: '9', definition: '' },
+            { value: 'RDE717.9', name: '10', definition: '' },
+            { value: 'RDE717.10', name: '11', definition: '' },
+            { value: 'RDE717.11', name: '12', definition: '' },
+          ],
+        },
+      },
+    ],
+    references: [],
+  };
+
+  // Load an external file
+  it('Should load real data from RadElement', () => {
+    const parsed = cdeSetSchema.safeParse(ribFractureJson);
+    if (!parsed.success) throw new Error(parsed.error.message);
+    const cdeSetData: CdeSetData = parsed.data;
+    const cdeSet = new CdeSet(cdeSetData);
+    expect(cdeSet).toHaveProperty('id', 'RDES112');
   });
 });
