2ca7a5dc81a24ed52b4220ca758e93b27bca4fd0
Kyle Macri <kmacri@bu.edu>
2024-01-24 16:22:55 +0000

    CdeSetBuilder work on numeric attribute

--- packages/cde_set/src/types/cdeSet.ts
+++ packages/cde_set/src/types/cdeSet.ts
@@ -187,7 +187,7 @@ export class CdeSetBuilder {
     // if numeric map to a float, int element
     if (this._data instanceof FindingModel) {
       const finding: FindingModel = this._data;
-      let element: Partial<CdElement>;  
+      let element: Partial<CdElement>;
       // for each attribute we need 1 element.
       finding.attributes.forEach((attribute) => {
         // check the type (choice or numeric)
@@ -197,7 +197,7 @@ export class CdeSetBuilder {
             //initialize values
             this._values = [];
             //type assertion
-            attribute = attribute as ChoiceAttribute; //TODO: why not working
+            attribute = attribute as ChoiceAttribute; //TODO: type assertion not working or need to add getter?
             //get each finding.attribute.values and convert to cdElement.values
             attribute.values.forEach((value) => {
               this._values.push({
@@ -210,93 +210,92 @@ export class CdeSetBuilder {
               id: 'Unique ID', // TODO: generate a unique ID
               parent_id: 1, // TODO: what number
               name: this._data.name,
-              short_name: '', // TODO: Needed?
-              editor: '', // TODO: add editor
-              instructions: '', // TODO: add instructions
-              synonyms: '', // TODO: add synonyms
+              short_name: undefined, //undefined
+              editor: undefined,
+              instructions: undefined,
+              synonyms: undefined,
               definition: this._data.description,
               question: undefined,
               version: {
                 name: 'Initial Version', //Required
                 version_date: new Date().toISOString(),
-                status_date: null,
+                status_date: undefined,
                 status: 'Proposed', //Required
               },
-              index_codes: null, //Optional 
-              authors: null, //{
+              index_codes: undefined, //Optional
+              authors: undefined, //{
               //   person: null,
               //   organization: null,
               // },
-              history: null, //No history its the first
-              specialty: null, //optional
-              references: null, //optional
-              source: null, //optional
+              history: undefined, //No history its the first
+              specialty: undefined, //optional
+              references: undefined, //optional
+              source: undefined, //optional
               value_set: {
-                 cardinality: {
-                   min_cardinality: 1,
-                   max_cardinality: 1,
-                 },
-                 value_min_max: {
-                   value_min: null, // optional
-                   value_max: null,// optional
-                 },
-                 step_value: null,//,
-                 unit: null, //,
-                 value_type: 'valueSet',
-                 value_size: null, //,
-              //
-              //   TODO: add loop here for each? then add it?
-              //   Have values be empty to start
-              values: this._values, //TODO values is part of value set
-            }
-          };
+                cardinality: {
+                  min_cardinality: 1,
+                  max_cardinality: 1,
+                },
+                value_min_max: {
+                  value_min: undefined, //optional/nullable
+                  value_max: undefined,
+                },
+                step_value: undefined,
+                unit: undefined, //,
+                value_type: 'valueSet',
+                value_size: undefined,
+                values: this._values,
+              },
+            };
 
             this._elements.push(element); // push the finding to the elements array?
             break;
 
           case 'numeric':
-            // numeric logic
-            //type assertion
-            attribute = attribute as NumericAttribute; //TODO: why type assertion not working
+            attribute = attribute as NumericAttribute;
             element = {
-              id: 'Unique ID', // TODO: generate a unique ID
-              parent_id: 1, // TODO: what number
+              id: 'Unique ID',
+              parent_id: 1,
               name: this._data.name,
-              short_name: '', // TODO: Needed?
-              editor: '', // TODO: add editor
-              instructions: '', // TODO: add instructions
-              synonyms: '', // TODO: add synonyms
+              short_name: undefined, //undefined
+              editor: undefined,
+              instructions: undefined,
+              synonyms: undefined,
               definition: this._data.description,
               question: undefined,
               version: {
-                name: '',
-                version_date: '',
-                status_date: '',
-                status: 'Proposed',
+                name: 'Initial Version', //Required
+                version_date: new Date().toISOString(),
+                status_date: undefined,
+                status: 'Proposed', //Required
               },
-              // index_codes: null,
-              // authors: {
+              index_codes: undefined, //Optional
+              authors: undefined, //{
               //   person: null,
               //   organization: null,
               // },
-              // history: ,
-              // specialty:
-              // references:
-              // source= //,
-              // integer_values: {
-              //   cardinality: {
-              //     min_cardinality: '',
-              //     max_cardinality: '',
-              //   },
-              //   value_min_max: {
-              //     value_min: //,
-              //     value_max: //,
-              //   },
-              //   step_value: //,
-              //   unit: //,
-              //   value_type: 'integer',
-              //   value_size:  //,
-              //   values: this._values,
+              history: undefined, //No history its the first
+              specialty: undefined, //optional
+              references: undefined, //optional
+              source: undefined, //optional
+              value_set: {
+                cardinality: {
+                  min_cardinality: 1,
+                  max_cardinality: 1,
+                },
+                value_min_max: {
+                  value_min: attribute.min,
+                  value_max: attribute.max,
+                },
+                step_value: undefined, //,
+                unit: undefined, //,
+                value_type: 'valueSet',
+                value_size: undefined, //,
+                //
+                //   TODO: add loop here for each? then add it?
+                //   Have values be empty to start
+                values: this._values, //TODO values is part of value set
+              },
             };
 
             break;
